35a7e57a5c7912544bc183440efe3e733da8ceed
sukesan1984 <takamikousuke@gmail.com>
2020-07-22 10:42:22 +0900

    Modify json-unmarshal for reading ary

--- golang/json-unmarshal.go
+++ golang/json-unmarshal.go
@@ -17,12 +17,26 @@ var str string = `{
 	"Body":"Hello",
 	"Time":1294706395881547000
 	}`
+var strAry string = `[{
+	"Name":"Alice",
+	"Body":"Hello",
+	"Time":1294706395881547000
+	}]`
 
 func main() {
 	var m Message
+	var ma []Message
 	b := []byte(str)
 	if err := json.Unmarshal(b, &m); err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("%s: %s\n", m.Name, m.Body)
+
+	if err := json.Unmarshal([]byte(strAry), &ma); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(ma)
+	for _, i := range ma {
+		fmt.Printf("%s: %s\n", i.Name, i.Body)
+	}
 }
