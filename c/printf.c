a6e64d13ed97fd9bcab276479427fdf5ea5ab1a2
Sukesan1984 <takamikousuke@gmail.com>
2020-08-01 12:36:33 +0900

    Update snprintf() example

--- c/printf.c
+++ c/printf.c
@@ -11,13 +11,20 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int	main()
 {
 	char	buf[4000];
+	char	*short_str;
 
 	snprintf(buf, 4000, "hoge %d\n", 100);
 	printf("%s", buf);
 	printf("%2$d, %3$s, %1$d\n", 1, 2, "hoge");
+
+	short_str = malloc(10 * sizeof(char));
+	snprintf(short_str, 12, "abcdefghijklmn");
+	printf("%s\n", short_str);
+
 	return (0);
 }
