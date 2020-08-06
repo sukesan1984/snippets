7a07661c78591b7ba843ec3d2383604e1a1ba4e0
Sukesan1984 <takamikousuke@gmail.com>
2020-08-06 20:51:25 +0900

    Update va_arg

--- c/va_arg.c
+++ c/va_arg.c
@@ -30,10 +30,27 @@ void test(char *fmt, ...)
 	va_end(ap);
 }
 
+void test_double(char *fmt, ...)
+{
+	va_list	ap;
+	double	i;
+	int		j;
+
+	va_start(ap, fmt);
+	i = va_arg(ap, double);
+	printf("%f\n", i);
+
+	j = va_arg(ap, int);
+	printf("%d\n", j);
+
+	va_end(ap);
+}
+
 int main()
 {
 	test("hoge", 1.23, 4);
 	test("hoge", 1.43, 1.24, 42);
 	test("hoge", 42);
+	test_double("hoge", 42, 10, 4.32);
 	return (0);
 }
