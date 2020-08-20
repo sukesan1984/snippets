961b9603dea99eaee9c0823d1af2b308f1f3b9fe
sukesan1984 <takamikousuke@gmail.com>
2020-08-20 15:11:36 +0900

    Modify json unmarshal

--- golang/json-unmarshal.go
+++ golang/json-unmarshal.go
@@ -12,6 +12,10 @@ type Message struct {
 	Time int64
 }
 
+type Number struct {
+	Num *int64
+}
+
 var str string = `{
 	"Name":"Alice",
 	"Body":"Hello",
@@ -21,6 +25,14 @@ var strAry string = `[{
 	"Name":"Alice",
 	"Body":"Hello",
 	"Time":1294706395881547000
+	},{
+	"Name":"Bob",
+	"Body":"Goodbye"
+	}]`
+
+var str2Ary string = `[{
+	"Num": 1
+	},{
 	}]`
 
 func main() {
@@ -30,13 +42,21 @@ func main() {
 	if err := json.Unmarshal(b, &m); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s: %s\n", m.Name, m.Body)
+	fmt.Printf("%s: %s, %d\n", m.Name, m.Body, m.Time)
 
 	if err := json.Unmarshal([]byte(strAry), &ma); err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(ma)
 	for _, i := range ma {
-		fmt.Printf("%s: %s\n", i.Name, i.Body)
+		fmt.Printf("%s: %s, %d\n", i.Name, i.Body, i.Time)
+	}
+
+	var na []Number
+	if err := json.Unmarshal([]byte(str2Ary), &na); err != nil {
+		log.Fatal(err)
+	}
+	for _, i := range na {
+		fmt.Printf("num: %d\n", i.Num)
 	}
 }
