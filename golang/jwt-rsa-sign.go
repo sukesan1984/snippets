700dbeb194e354bcbb78e01d67a1cc9e8ec975b4
sukesan1984 <takamikousuke@gmail.com>
2020-08-25 13:57:36 +0900

    Update jwt sign

--- golang/jwt-rsa-sign.go
+++ golang/jwt-rsa-sign.go
@@ -8,6 +8,12 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
+const (
+	iatKey   = "iat"
+	expKey   = "exp"
+	lifetime = 1 * time.Minute
+)
+
 func sign() (string, error) {
 	signBytes, err := ioutil.ReadFile("./key.pem")
 	if err != nil {
@@ -17,9 +23,12 @@ func sign() (string, error) {
 	if err != nil {
 		panic(err)
 	}
+	now := time.Now()
 	token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
-		"foo": "bar",
-		"nbf": time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
+		iatKey: now.Unix(),
+		expKey: now.Add(lifetime).Unix(),
+		"foo":  "bar",
+		"nbf":  time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
 	})
 	return token.SignedString(signKey)
 }
