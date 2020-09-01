6ffbf557df383f04dc14787e474aaabac3704a73
sukesan1984 <takamikousuke@gmail.com>
2020-09-01 11:27:02 +0900

    Modify time

--- golang/time.go
+++ golang/time.go
@@ -7,6 +7,9 @@ import (
 )
 
 func main() {
+	var seconds int64
+	seconds = 10
 	minute := 60 * time.Minute
 	fmt.Println(reflect.TypeOf(minute))
+	fmt.Println(time.Duration(seconds) * time.Second)
 }
