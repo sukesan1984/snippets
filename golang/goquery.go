917a5d3678ce2be9454e1397a6461a9b63ce2095
sukesan1984 <takamikousuke@gmail.com>
2020-10-15 14:37:26 +0900

    Add goquery example

---
+++ golang/goquery.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func ExampleScrape() {
+	res, err := http.Get("http://metalsucks.net")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Find the review items
+	doc.Find(".sidebar-reviews article .content-block").Each(func(i int, s *goquery.Selection) {
+		band := s.Find("a").Text()
+		title := s.Find("i").Text()
+		fmt.Printf("Review %d: %s - %s\n", i, band, title)
+	})
+}
+
+func main() {
+	ExampleScrape()
+}
