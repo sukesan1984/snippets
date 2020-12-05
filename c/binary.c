3c63c881594bb02864e98eb0448dc5234c401452
Sukesan1984 <takamikousuke@gmail.com>
2020-12-05 17:57:42 +0900

    Add binary.c

--- c/binary.c
+++ c/binary.c
@@ -6,7 +6,7 @@
 /*   By: ktakami <ktakami@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/05 15:26:04 by ktakami           #+#    #+#             */
-/*   Updated: 2020/12/05 16:43:43 by ktakami          ###   ########.fr       */
+/*   Updated: 2020/12/05 17:55:20 by ktakami          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,23 +49,25 @@ t_data	create_data(int data, int bytes)
 	return (d);
 }
 
-t_bmp	initialize()
+t_bmp	initialize(int width, int height)
 {
 	t_bmp	bmp_data;
 
 	bmp_data.filetype = create_data(0x424d, 2);
-	bmp_data.filesize = create_data(126, 4);
+	bmp_data.filesize = create_data(width*4*height+54, 4);
 	bmp_data.prepared = create_data(0, 4);
-	bmp_data.offset = create_data(62, 4);
+	//bmp_data.offset = create_data(62, 4);
+	bmp_data.offset = create_data(54, 4);
 	bmp_data.header = create_data(40, 4);
-	bmp_data.width	= create_data(16, 4);
-	bmp_data.height	= create_data(16, 4);
+	bmp_data.width	= create_data(width, 4);
+	bmp_data.height	= create_data(height, 4);
 	bmp_data.plane = create_data(1, 2);
-	bmp_data.color_bit = create_data(1, 2);
+	bmp_data.color_bit = create_data(32, 2);
 	bmp_data.comptype = create_data(0, 4);
-	bmp_data.img_size = create_data(0x40, 4);
-	bmp_data.res_width = create_data(0x10, 4);
-	bmp_data.res_height = create_data(0x10, 4);
+	//bmp_data.img_size = create_data(0x40, 4);
+	bmp_data.img_size = create_data(width*4*height, 4);
+	bmp_data.res_width = create_data(0, 4);
+	bmp_data.res_height = create_data(0, 4);
 	bmp_data.pallet_size = create_data(0, 4);
 	bmp_data.important_color = create_data(0, 4);
 	return (bmp_data);
@@ -100,7 +102,7 @@ void	write_recursive(int fd, int data)
 	if (data / 256)
 		write_recursive(fd, data / 256);
 	c = data % 256;
-	printf("data: %d\n", c);
+	//printf("data: %d\n", c);
 	write(fd, &c, 1);
 }
 
@@ -111,7 +113,7 @@ void	write_data(int fd, int data, int bytes)
 
 	write_recursive(fd, data);
 	data_bytes = get_bytes(data);
-	printf("data_bytes: %d\n", data_bytes);
+	//printf("data_bytes: %d\n", data_bytes);
 	c = 0;
 	while ((bytes - data_bytes) > 0)
 	{
@@ -146,6 +148,8 @@ void	write_bmp(int fd, t_bmp bmp)
 
 int	main()
 {
+	int	width;
+	int	height;
 	char	*path;
 	char	*data;
 	int	fd;
@@ -159,27 +163,43 @@ int	main()
 		exit(1);
 	}
 	//data = "## ファイルヘッダ ##\n42 4d\n7e 00 00 00\n00 00\n00 00\n3e 00 00 00\n## 情報ヘッダ ##\n28 00 00 00\n10 00 00 00\n10 00 00 00\n01 00\n01 00\n00 00 00 00\n40 00 00 00\n10 00 00 00\n10 00 00 00\n00 00 00 00\n00 00 00 00\n## パレットデータ ##\nff ff ff 00\n00 00 00 00\n\n00 00 00 00\n7f fe 00 00\n40 02 00 00\n40 02 00 00\n40 02 00 00\n40 02 00 00\n40 02 00 00\n40 02 00 00\n40 02 00 00\n40 02 00 00\n40 02 00 00\n40 02 00 00\n40 02 00 0040 02 00 00\n7f fe 00 00\n00 00 00 00";
+	width = 64;
+	height = 32;
 
-	bmp = initialize();
+	bmp = initialize(width, height);
 	write_bmp(fd, bmp);
-	write_data(fd, 0xffffff, 4);
-	write_data(fd, 0x0, 4);
-	write_data(fd, 0x0, 4);
-	write_data(fd, 0x7ffe, 4);
-	write_data(fd, 0x4002, 4);
-	write_data(fd, 0x4002, 4);
-	write_data(fd, 0x4002, 4);
-	write_data(fd, 0x4002, 4);
-	write_data(fd, 0x4002, 4);
-	write_data(fd, 0x4002, 4);
-	write_data(fd, 0x4002, 4);
-	write_data(fd, 0x4002, 4);
-	write_data(fd, 0x4002, 4);
-	write_data(fd, 0x4002, 4);
-	write_data(fd, 0x4002, 4);
-	write_data(fd, 0x4002, 4);
-	write_data(fd, 0x7ffe, 4);
-	write_data(fd, 0x0, 4);
+	//write_data(fd, 0xffffff, 4);
+	//write_data(fd, 0x0, 4);
+	for (int j = 0; j < height; j++)
+	{
+		for (int i = 0; i < width; i++)
+		{
+			if (i == 1 || j == 1 || i == width - 2|| j == height - 2)
+			{
+				write_data(fd, 0x000ff00, 4);
+			}
+			else
+			{
+				write_data(fd, 0x00ffffff, 4);
+			}
+		}
+	}
+	//write_data(fd, 0x0, 4);
+	//write_data(fd, 0x7ffe, 4);
+	//write_data(fd, 0x4002, 4);
+	//write_data(fd, 0x4002, 4);
+	//write_data(fd, 0x4002, 4);
+	//write_data(fd, 0x4002, 4);
+	//write_data(fd, 0x4002, 4);
+	//write_data(fd, 0x4002, 4);
+	//write_data(fd, 0x4002, 4);
+	//write_data(fd, 0x4002, 4);
+	//write_data(fd, 0x4002, 4);
+	//write_data(fd, 0x4002, 4);
+	//write_data(fd, 0x4002, 4);
+	//write_data(fd, 0x4002, 4);
+	//write_data(fd, 0x7ffe, 4);
+	//write_data(fd, 0x0, 4);
 
 	return (0);
 }
